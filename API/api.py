51dfc9d739218c87589b706dfb5e92b979b5f27c
Paul <paul.monnery@epitech.eu>
2019-05-06 16:31:00 +0200

    added log writing

--- API/api.py
+++ API/api.py
@@ -1,8 +1,21 @@
 #!/bin/python3
 from flask import *
-import csv
+import csv, datetime, sys
 
 app = Flask(__name__)
+now = datetime.datetime.now()
+
+
+def write_log_line(data, is_granted):
+        file = open('workshop_log.txt', 'a')
+        if len(data) > 8:
+                file.write(is_granted + "\t: " + data + ":\t" + str(now.year)
+                + ":" + str(now.month) + ":" + str(now.day) + "  "+ str(now.hour)
+                + ":" + str(now.minute) + ":" + str(now.second) + "\n")
+        else:
+                file.write(is_granted + "\t: " + data + ":\t\t" + str(now.year)
+                + ":" + str(now.month) + ":" + str(now.day) + "  " + str(now.hour)
+                + ":" + str(now.minute) + ":" + str(now.second) + "\n")
 
 @app.route("/", methods = ["POST", "GET"])
 def receiveUID():
@@ -14,7 +27,9 @@ def receiveUID():
                 reader = csv.reader(file)
                 for row in reader:
                         if row[0] == data:
+                                write_log_line(data, "ACCES GRANTED")
                                 return ("ACCES GRANTED", 200)
         finally:
                 file.close()
+        write_log_line(data, "ACCES GRANTED")
         return ("ACCES DENIED", 404)
